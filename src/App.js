f9468a2048ed3af2dbec9524fdf97707a7b9aa86
hyeonjae4012 <jung.hyeonjae@classmethod.jp>
2023-01-12 16:24:46 +0900

    add button onclick event

--- src/App.js
+++ src/App.js
@@ -3,35 +3,23 @@ import './App.css';
 
 function App() {
 
-  // const upsertItem = () => {
-  //   console.log("upsertItem")
-  // }
+  const upsertItem = () => {
+    console.log("upsertItem")
+  }
 
-  // const getItem = () => {
-  //   console.log("getItem")
-  // }
+  const getItem = () => {
+    console.log("getItem")
+  }
 
-  // const deleteItem = () => {
-  //   console.log("deleteItem")
-  // }
-
+  const deleteItem = () => {
+    console.log("deleteItem")
+  }
 
   return (
-    <div className="TEST">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Cloudfront Test
-        </a>
-      </header>
+    <div className="APP">
+      <button onClick={ upsertItem }>Upsert Item</button>
+      <button onClick={ getItem }>get Item</button>
+      <button onClick={ deleteItem }>delete Item</button>
     </div>
   );
 }
