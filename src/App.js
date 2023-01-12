66e67a1b9f8af49648ad5b52da1453e2f208f81e
hyeonjae4012 <jung.hyeonjae@classmethod.jp>
2023-01-12 16:43:51 +0900

    parameter test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,66 @@
 import './App.css';
+import axios from 'axios';
 
 function App() {
 
-  const upsertItem = () => {
-    console.log("upsertItem")
-  }
+  const base_url = "hyeonjae.classmethod.info"
+
+  const createItem = () => {
+    console.log("createItem");
+    axios.POST(`${base_url}/test`, {
+      Key: "test"
+    })
+    .then((output) => {
+      console.log(output);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  };
+
+  const updateItem = () => {
+    console.log("updateItem");
+    axios.put(`${base_url}/test`, {
+      Key: "test"
+    })
+    .then((output) => {
+      console.log(output);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  };
 
   const getItem = () => {
-    console.log("getItem")
-  }
+    console.log("upsertItem");
+    axios.get(`${base_url}/test`, {
+      Key: "test"
+    })
+    .then((output) => {
+      console.log(output);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  };
 
   const deleteItem = () => {
-    console.log("deleteItem")
-  }
+    console.log("upsertItem");
+    axios.delete(`${base_url}/test`, {
+      Key: "test"
+    })
+    .then((output) => {
+      console.log(output);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  };
 
   return (
     <div className="APP">
-      <button onClick={ upsertItem }>Upsert Item</button>
+      <button onClick={ createItem }>Create Item</button>
+      <button onClick={ updateItem }>Update Item</button>
       <button onClick={ getItem }>get Item</button>
       <button onClick={ deleteItem }>delete Item</button>
     </div>
