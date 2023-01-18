9b5b37b2d1728a2a7667f9a1c24f51c4caf1d892
hyeonjae4012 <jung.hyeonjae@classmethod.jp>
2023-01-18 13:56:22 +0900

    immediately change for blog

--- src/App.js
+++ src/App.js
@@ -16,55 +16,55 @@ function App() {
     });
   };
 
-  const updateItem = () => {
-    console.log("updateItem");
-    axios.put(`/api/test`, {
-      TestPartitionKey: "test"
-    })
-    .then((output) => {
-      console.log(output);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  };
+  // const updateItem = () => {
+  //   console.log("updateItem");
+  //   axios.put(`/api/test`, {
+  //     TestPartitionKey: "test"
+  //   })
+  //   .then((output) => {
+  //     console.log(output);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+  // };
 
-  const getItem = () => {
-    console.log("getItem");
-    axios.get(`/api/test`, {
-      params: {
-        TestPartitionKey: "test"
-      }
-    })
-    .then((output) => {
-      console.log(output);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  };
+  // const getItem = () => {
+  //   console.log("getItem");
+  //   axios.get(`/api/test`, {
+  //     params: {
+  //       TestPartitionKey: "test"
+  //     }
+  //   })
+  //   .then((output) => {
+  //     console.log(output);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+  // };
 
-  const deleteItem = () => {
-    console.log("deleteItem");
-    axios.delete(`/api/test`, {
-      params: {
-        TestPartitionKey: "test"
-      }
-    })
-    .then((output) => {
-      console.log(output);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  };
+  // const deleteItem = () => {
+  //   console.log("deleteItem");
+  //   axios.delete(`/api/test`, {
+  //     params: {
+  //       TestPartitionKey: "test"
+  //     }
+  //   })
+  //   .then((output) => {
+  //     console.log(output);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+  // };
 
   return (
     <div className="APP">
-      <button onClick={ createItem }>Create Item</button>
-      <button onClick={ updateItem }>Update Item</button>
+      <button onClick={ createItem }>Axios Test</button>
+      {/* <button onClick={ updateItem }>Update Item</button>
       <button onClick={ getItem }>get Item</button>
-      <button onClick={ deleteItem }>delete Item</button>
+      <button onClick={ deleteItem }>delete Item</button> */}
     </div>
   );
 }
